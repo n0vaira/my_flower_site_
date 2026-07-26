ed0ebc4ff5bc45277bb35760876ce7aa0a4e6242
n0vaira <irinabolsakova82@gmail.com>
2026-07-27 01:21:28 +0900

    форма для кнопки заказа

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const btnONas = document.getElementById('btn-o-nas');
 const infoBlock = document.getElementById('o-nas-info');
 const closeBtn = document.getElementById('close-o-nas');
+const btnZakazat = document.getElementById('btn-zakazat');
+const zakazatModal = document.getElementById('zakazat-modal');
+const closeZakazat = document.getElementById('close-zakazat');
+const zakazatForm = document.getElementById('zakazat-form');
+
 
 btnONas.addEventListener('click', function(){
     infoBlock.classList.toggle('hidden');
@@ -8,4 +13,19 @@ btnONas.addEventListener('click', function(){
 
 closeBtn. addEventListener('click', function(){
     infoBlock.classList.add('hidden');
+});
+
+
+btnZakazat.addEventListener('click', function(){
+    zakazatModal.classList.toggle('hidden');
+});
+
+closeZakazat.addEventListener('click', function(){
+    zakazatModal.classList.add('hidden');
+});
+
+zakazatForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    zakazatForm.reset();
+    zakazatModal.classList.add('hidden');
 });
