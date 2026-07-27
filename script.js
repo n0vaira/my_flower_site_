caaed10600221425227cf9a343bd747854abefd7
n0vaira <irinabolsakova82@gmail.com>
2026-07-27 22:34:12 +0900

    добавление формы для последней кнопки

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 const btnONas = document.getElementById('btn-o-nas');
 const infoBlock = document.getElementById('o-nas-info');
 const closeBtn = document.getElementById('close-o-nas');
-const btnZakazat = document.getElementById('btn-zakazat');
-const zakazatModal = document.getElementById('zakazat-modal');
-const closeZakazat = document.getElementById('close-zakazat');
-const zakazatForm = document.getElementById('zakazat-form');
-
 
 btnONas.addEventListener('click', function(){
     infoBlock.classList.toggle('hidden');
@@ -15,6 +10,10 @@ closeBtn. addEventListener('click', function(){
     infoBlock.classList.add('hidden');
 });
 
+const btnZakazat = document.getElementById('btn-zakazat');
+const zakazatModal = document.getElementById('zakazat-modal');
+const closeZakazat = document.getElementById('close-zakazat');
+const zakazatForm = document.getElementById('zakazat-form');
 
 btnZakazat.addEventListener('click', function(){
     zakazatModal.classList.toggle('hidden');
@@ -28,4 +27,21 @@ zakazatForm.addEventListener('submit', function(event){
     event.preventDefault();
     zakazatForm.reset();
     zakazatModal.classList.add('hidden');
+});
+
+const btnBuket = document.getElementById('btn-buket');
+const btnModal = document.getElementById('buket-modal');
+const btnClose = document.getElementById('close-buket');
+const buketForm = document.getElementById('buket-form');
+
+btnBuket.addEventListener('click', function(){
+    btnModal.classList.toggle('hidden');
+});
+btnClose.addEventListener('click', function(){
+    btnModal.classList.add('hidden');
+});
+buketForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    buketForm.reset();
+    btnModal.classList.add('hidden');
 });
